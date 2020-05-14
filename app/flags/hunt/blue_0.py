b3017cd38b056bfb94a4ecdc03f9e3411263b873
garunagiri <garunagiri@mitre.org>
2020-05-14 12:06:18 -0400

    making them hidden again

--- app/flags/hunt/blue_0.py
+++ app/flags/hunt/blue_0.py
@@ -15,11 +15,11 @@ async def prerequisites_met(services):
 async def setup(services):
     access = dict(access=[BaseWorld.Access.RED])
     data = dict(name='Hunt0', group='cert-nix', adversary_id='6e19d5f9-e4d9-42b2-bc89-ea4298541d80',
-                planner='sequential', atomic_enabled=1, auto_close=1, hidden=False)
+                planner='sequential', atomic_enabled=1, hidden=True)
     return (await services.get('rest_svc').create_operation(access=access, data=data))[0]['id']
 
 
 async def verify(services, operation_id):
     operation = (await services.get('data_svc').locate('operations', match=dict(id=operation_id)))[0]
-    return operation.finish and len(operation.chain) == 1 and \
+    return len(operation.chain) == 1 and \
         'host.user.name' in [f.trait for f in operation.all_facts()]
